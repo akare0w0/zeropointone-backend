e5de2e35bb243740f2c52de4b7879697a3e3ec8c
XiaoYuawa <2147279898@qq.com>
2023-01-27 00:28:03 +0800

    commit

--- app/db/data_filter.py
+++ app/db/data_filter.py
@@ -17,4 +17,14 @@ def is_vaild_strs(strings: List[str]) -> bool:
     return True
 
 def isVaildEmail(string: str):
+    if not string.find("@"): #检测是否含有@
+        return False
+    elif len(string.split("@")) != 2: #检测是否有多个@
+        return False
+    elif string[-1] == "@": #检测是否以@结尾
+        return False
+    elif not string.find("."): #检测是否含 .
+        return False
+    elif string[string.find("@") + 1] == ".": #检测@后面是否为 .
+        return False
     return True
--- app/methods/video/search.py
+++ app/methods/video/search.py
@@ -5,4 +5,4 @@ router = APIRouter(prefix='/search')
 
 @router.get("/")
 def search(keyword: str):
-    col = get_db().get_collection('keyword')
+    pass
