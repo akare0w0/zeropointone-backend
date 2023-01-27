08f6ad8a2b4fb8ad591f00bb8d92780de7d2efd0
Helloworldzz <2582697178@qq.com>
2023-01-27 15:00:16 +0800

    b

--- app/db/db.py
+++ app/db/db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-url = 'mongodb://jp-tyo-ntt-1.natfrp.cloud:16412'
+url = 'mongodb://jp-tyo-ntt-1.natfrp.cloud:16412';
 
 def get_client() -> MongoClient:
     return MongoClient(url)
